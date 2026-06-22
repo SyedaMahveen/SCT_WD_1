da543aa9bb3ead597375a916bf7870e94317988a
SyedaMahveen <mahveens650@gmail.com>
2026-06-22 11:59:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-window.addEventListener("scroll", function() {
+const navbar = document.getElementById("navbar");
+const topBtn = document.getElementById("topBtn");
 
-    let navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
 
-    if(window.scrollY > 50){
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
+if(window.scrollY > 50){
+navbar.style.background = "#2563eb";
+}
 
+else{
+navbar.style.background = "white";
+}
+
+if(window.scrollY > 300){
+topBtn.style.display = "block";
+}
+else{
+topBtn.style.display = "none";
+}
+
+});
+
+topBtn.addEventListener("click", () => {
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
 });
